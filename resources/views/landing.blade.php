52f0f5bd78008fc7e7d29353002be2f3167265e1
Muhamad iqbal <mirfmaster@gmail.com>
2019-07-29 19:45:19 +0700

    initial

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/landing.css')}}">
+    <title>Verikool</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img src="{{url('images/logo-finnet.png')}}" alt="not found" height="3%"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse pull-right" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{url('images/slide1.jpg')}}" class="d-block w-100" alt="...">
+
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{url('images/slide1.jpg')}}" class="d-block w-100" alt="...">
+
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{url('images/slide1.jpg')}}" class="d-block w-100" alt="...">
+
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <div class="blank-space"></div>
+    <div id="king-229250" class="sponsor">
+        <div class="container " style="">
+            <div class="vc_col-sm-3 wpb_column vc_column_container ">
+                <div class="wpb_wrapper">
+
+                </div>
+            </div>
+
+            <div class="vc_col-sm-6 wpb_column vc_column_container ">
+                <div class="wpb_wrapper">
+                    <div class="clearfix mt-12"></div>
+                    <h2 style="text-align: center" class="vc_custom_heading">Plugin &amp; API</h2>
+                    <div class="wpb_text_column wpb_content_element" style="text-align:center">
+                        <div class="wpb_wrapper">
+                            <div class="lshowcase-clear-both">&nbsp;</div>
+                            <div class="lshowcase-logos">
+                                <div>
+                                    <div class="logo">
+                                        <div class="lshowcase-wrap-responsive">
+                                            <div class="lshowcase-boxInner"><img src="https://finpay.co.id/wp-content/uploads/2016/02/Magento_logo-200x63.png" alt="magento logo" title="" class="lshowcase-normal"></div>
+                                        </div>
+                                    </div>
+                                    <div class="logo ">
+                                        <div class="lshowcase-wrap-responsive">
+                                            <div class="lshowcase-boxInner"><img src="https://finpay.co.id/wp-content/uploads/2016/02/opencart-logo_400x300-200x150.png" height="80px" width="180px" alt="Opencart" title="" class="lshowcase-normal"></div>
+                                        </div>
+                                    </div>
+                                    <div class="logo ">
+                                        <div class="lshowcase-wrap-responsive">
+                                            <div class="lshowcase-boxInner"><img src="https://finpay.co.id/wp-content/uploads/2016/02/prestashop-200x46.png" alt="Prestashop" title="" class="lshowcase-normal"></div>
+                                        </div>
+                                    </div>
+                                    <div class="logo ">
+                                        <div class="lshowcase-wrap-responsive">
+                                            <div class="lshowcase-boxInner"><img src="https://finpay.co.id/wp-content/uploads/2016/02/virtuemart_slogan_blue-200x59.png" alt="Virtue Mart" title="" class="lshowcase-normal"></div>
+                                        </div>
+                                    </div>
+                                    <div class="logo ">
+                                        <div class="lshowcase-wrap-responsive">
+                                            <div class="lshowcase-boxInner"><img src="https://finpay.co.id/wp-content/uploads/2016/02/woocommerce-logo-200x42.png" alt="Woocommerce" title="" class="lshowcase-normal"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lshowcase-clear-both" style="clear:both">&nbsp;</div>
+
+                            <a class="btn btn-danger" href="https://finpay.co.id/finpay/payment-api/" title=""><i class="vc_btn3-icon entypo-icon entypo-icon-play"></i> Lihat Detail</a>
+                    <div class="clearfix margin_top7"></div>
+                        </div>
+                    </div>
+                    <div class="clearfix margin_top5"></div>
+                </div>
+            </div>
+
+            <div class="vc_col-sm-3 wpb_column vc_column_container ">
+                <div class="wpb_wrapper">
+
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+
+    <hr width="75%" style="border:solid 0.6px gray">
+    <div class="container" style="margin-bottom:10%">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>
+                    Facebook finpay
+                </h3>
+            </div>
+            <div class="col-md-4">
+                <h3>
+                    Twitter finpay
+                </h3>
+            </div>
+            <div class="col-md-4">
+                <h3>
+                    News finpay
+                </h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="gray-space">
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
